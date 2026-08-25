17788a32311d8e57767e045303dd4cca36d22eee
Zach Harel <zach@zharel.me>
2026-08-24 22:46:01 -0400

    split nextftc into v1 and v2 (#5)
    * split nextftc into v1 and v2
    
    * add default v2 version
    
    * refactor: update NextV2 to use defaultVersion for artifact versioning
    
    * Update README to remove nextftc v2 version requirement

--- FTCLibraries/src/main/kotlin/dev/frozenmilk/libs/Next.kt
+++ FTCLibraries/src/main/kotlin/dev/frozenmilk/libs/Next.kt
@@ -5,30 +5,56 @@ import dev.frozenmilk.easyautolibraries.EasyAutoDependency
 import dev.frozenmilk.easyautolibraries.EasyAutoScope
 
 @Suppress("unused")
-class Next(ftc: FTC) : EasyAutoScope<Next>(ftc) {
-    private fun library(version: String) = dependency { name ->
-        EasyAutoDependency(
-            group = "dev.nextftc",
-            artifact = name,
-            version = version,
-        )
+class Next(val ftcProject: FTC) : EasyAutoScope<Next>(ftcProject) {
+    val v1 = NextV1()
+
+    inner class NextV1 internal constructor() : EasyAutoScope<NextV1>(ftcProject) {
+        private fun library(version: String) = dependency { name ->
+            EasyAutoDependency(
+                group = "dev.nextftc",
+                artifact = name,
+                version = version,
+            )
+        }
+
+        val ftc by library("1.0.1")
+        val hardware by library("1.0.1")
+        val control by library("1.0.0")
+        val bindings by library("1.0.1")
+
+        private fun extension(version: String) = dependency { name ->
+            EasyAutoDependency(
+                group = "dev.nextftc.extensions",
+                artifact = name,
+                version = version,
+            )
+        }
+
+        val roadrunner by extension("1.0.0")
+        val pedro by extension("1.0.0")
+        val fateweaver by extension("1.0.0")
     }
 
-    val ftc by library("1.0.1")
-    val hardware by library("1.0.1")
-    val control by library("1.0.0")
-    val bindings by library("1.0.1")
+    val v2 = NextV2("0.1.0")
 
-    val control2 by library("0.0.2")
-    private fun extension(version: String) = dependency { name ->
-        EasyAutoDependency(
-            group = "dev.nextftc.extensions",
-            artifact = name,
-            version = version,
-        )
+    inner class NextV2 internal constructor(val version: String) : EasyAutoScope<NextV2>(ftcProject) {
+        private fun library(conflict: EasyAutoDependency? = null) = dependency { name ->
+            EasyAutoDependency(
+                group = "dev.nextftc.v2",
+                artifact = name,
+                defaultVersion = { version },
+            ) {
+                conflict?.let {
+                    incompatibleWith(
+                        it,
+                        "NextFTC v2 is incompatible with NextFTC v1"
+                    )
+                }
+            }
+        }
+
+        val control by library(v1.control)
+        val hardware by library(v1.hardware)
+        val robot by library(v1.ftc)
     }
-
-    val roadrunner by extension("1.0.0")
-    val pedro by extension("1.0.0")
-    val fateweaver by extension("1.0.0")
 }
